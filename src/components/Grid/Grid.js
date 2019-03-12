904e65a5a495ec184d4f6ce07a4f6e09b1fe1e5f
Kamil <developer.kamil90@gmail.com>
2019-03-12 15:51:03 +0100

    add logic for checking winning sequence

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -32,20 +32,109 @@ export default class Grid extends Component {
       return arr.slice();
     });
     // Check if there is no winner and if tile with null value exists
-    if (this.state.winner === "" && gridCopy[columnID].indexOf(null) !== -1) {
-      // place new tile on the board
-      let newMove = gridCopy[columnID].reverse();
-      newMove[newMove.indexOf(null)] = this.state.currentMove;
-      newMove.reverse();
-      this.setState({
-        grid: gridCopy,
-        currentMove: this.togglePlayer()
-      });
+    if (this.state.winner === "") {
+      if (gridCopy[columnID].indexOf(null) !== -1) {
+        // place new tile on the board
+        let newMove = gridCopy[columnID].reverse();
+        newMove[newMove.indexOf(null)] = this.state.currentMove;
+        newMove.reverse();
+        this.setState({
+          grid: gridCopy,
+          currentMove: this.togglePlayer()
+        });
+      }
     }
   };
+
+  winningSlice = (a, b, c, d) => {
+    return a !== null && a === b && a === c && a === d;
+  };
+  winVertical = board => {
+    // check if row is less than 4 index (we start count from index 0)
+    for (let c = 0; c < 7; c++)
+      for (let r = 0; r < 4; r++)
+        if (
+          this.winningSlice(
+            board[c][r],
+            board[c][r + 1],
+            board[c][r + 2],
+            board[c][r + 3]
+          )
+        )
+          return board[c][r];
+  };
+  winHorizontal = board => {
+    // check if column is less than 4 index (we start count from index 0)
+    for (let r = 0; r < 6; r++)
+      for (let c = 0; c < 4; c++)
+        if (
+          this.winningSlice(
+            board[c][r],
+            board[c + 1][r],
+            board[c + 2][r],
+            board[c + 3][r]
+          )
+        )
+          return board[c][r];
+  };
+  winDiagonalLeft = board => {
+    // check if column is less than 4 index (we start count from index 0)
+    for (let r = 0; r < 3; r++)
+      for (let c = 0; c < 4; c++)
+        if (
+          this.winningSlice(
+            board[c][r],
+            board[c + 1][r + 1],
+            board[c + 2][r + 2],
+            board[c + 3][r + 3]
+          )
+        )
+          return board[c][r];
+  };
+  winDiagonaRight = board => {
+    // check if row is less than 4 index (we start count from index 0)
+    for (let r = 0; r < 4; r++)
+      for (let c = 3; c < 6; c++)
+        if (
+          this.winningSlice(
+            board[c][r],
+            board[c - 1][r + 1],
+            board[c - 2][r + 2],
+            board[c - 3][r + 3]
+          )
+        )
+          return board[c][r];
+  };
+  checkDraw = board => {
+    for (let r = 0; r < 6; r++) {
+      for (let c = 0; c < 7; c++) {
+        if (board[r][c] === null) {
+          return null;
+        }
+      }
+    }
+    return "draw";
+  };
+  checkSequence = board => {
+    return (
+      this.winVertical(board) ||
+      this.winHorizontal(board) ||
+      this.winDiagonaRight(board) ||
+      this.winDiagonalLeft(board) ||
+      this.checkDraw(board)
+    );
+  };
+  componentDidUpdate = () => {
+    let winner = this.checkSequence(this.state.grid);
+    console.log(winner);
+  };
+
   render() {
     return (
       <React.Fragment>
+        <button className="btn" onClick={() => this.initializeBoard()}>
+          New Game
+        </button>
         <div className="grid">
           {this.state.grid.map((_, index) => {
             return (
